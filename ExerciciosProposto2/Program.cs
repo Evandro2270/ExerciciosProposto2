f97d9e6932fff089aa17ac90cb6dd2dd7bcc1293
Evandro2270 <evandro535@gmail.com>
2024-12-04 11:16:57 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosProposto2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+namespace Programa
+{
+    using System;
+
+    namespace URI
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                double R, A, Pi = 3.14159;
+
+                R = double.Parse(Console.ReadLine(), CI);
+                A = Pi * R * R;
+
+                Console.WriteLine("A= " + A.ToString("F4", CI));
+
+            }
+        }
+    }
+}
